746b8ce5a27b6b28f122d4bb2c425aad54103b2e
Mikhail Borysewic <mikhail.borysewic@gmail.com>
2022-11-27 21:48:20 +0300

    Add admin panel url

--- pybooker/bookapp/urls.py
+++ pybooker/bookapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
+from django.contrib import admin
 
 from . import views
 
 
 urlpatterns = [
     path("", views.IndexPageView.as_view(), name="index"),
+    path('admin/', admin.site.urls),
     path("authors/", views.AuthorsPageView.as_view(), name="authors"),
     path("authors/add/", views.AddAuthorPageView.as_view(), name="authors-add"),
     path("books/", views.BooksPageView.as_view(), name="books"),
