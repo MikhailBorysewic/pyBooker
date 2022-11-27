ebc9acfa2f29796b0c52bc69231de78245c1357d
Mikhail Borysewic <mikhail.borysewic@gmail.com>
2022-11-27 21:48:06 +0300

    Register models in admin panel.

--- pybooker/bookapp/admin.py
+++ pybooker/bookapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+
+admin.site.register(apps.get_model("bookapi", "Author"))
+admin.site.register(apps.get_model("bookapi", "Book"))
+admin.site.register(apps.get_model("bookapi", "Genre"))
+admin.site.register(apps.get_model("bookapi", "Publisher"))
+admin.site.register(apps.get_model("bookapi", "Quote"))
+admin.site.register(apps.get_model("bookapi", "Review"))
